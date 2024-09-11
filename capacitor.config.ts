9860e2bf33618851fe232f12cfe47bb1c5aec45d
codewithshey <aguinaldojershey@gmail.com>
2024-09-11 15:32:42 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fast-food-frontend',
+  webDir: 'dist'
+};
+
+export default config;
